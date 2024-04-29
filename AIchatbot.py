090af534510d6629a46a4b7a7f5991a6f71043ff
kaylee <kalikatka@yahoo.com>
2024-04-29 18:16:32 -0400

    AI chatbot created

--- AIchatbot.py
+++ AIchatbot.py
@@ -9,7 +9,7 @@ client = Client()
 
 chat_completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "Give me a compatibility ANALYSIS BETWEEN TAURUS AND A LIBRAA "}],
+    messages=[{"role": "user", "content": "Give me a compatibility ANALYSIS BETWEEN TAURUS AND A LIBRA"}],
 )
 
 print(chat_completion.choices[0].message.content or "")
